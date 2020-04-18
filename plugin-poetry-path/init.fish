c49650f1c4b8af49423d607a995edad6c4bcf6cd
Anand Balakrishnan <anandbala1597@gmail.com>
2020-04-18 16:09:55 -0700

    Add poetry

--- plugin-poetry-path/init.fish
+++ plugin-poetry-path/init.fish
@@ -1,5 +1,6 @@
-test -n $POETRY_HOME
-  or set -gx $POETRY_HOME $HOME/.poetry/bin
+if test -z "$POETRY_HOME"
+  set -gx POETRY_HOME $HOME/.poetry/bin
+end
 
 set -l poetry_path $POETRY_HOME
 
@@ -8,12 +9,4 @@ if test -d $poetry_path
   contains -- $poetry_path $PATH
     or set -gx PATH $poetry_path $PATH
 end
-
-set -l name (basename (status -f) .fish){_uninstall}
-
-function $name -e $name
-  set -l i (contains -i -- $poetry_path $PATH)
-    and set -e PATH[$i]
-end
-
 
